1ca39116f0367cb8c2b932626863c06dccb199ea
yujiro <YujiroS>
2022-06-23 16:13:31 +0200

    fixing images not showing on Github Page

--- docs/_site/assets/js/lunr/lunr-store.js
+++ docs/_site/assets/js/lunr/lunr-store.js
@@ -2,6 +2,6 @@ var store = [{
         "title": "Week 0",
         "excerpt":"Ready, set, go! With this first entry on my weekly blog, I start my master’s thesis with the objective and hope to learn and solve problems related to autonomous driving using computer vision. This first week, the focus is pointed towards installing and playing with softwares such as Gazebo (a...","categories": ["Weekly Log"],
         "tags": ["github page","ros noetic","gazebo"],
-        "url": "/weekly%20log/week-0/",
+        "url": "/2022-tfm-enrique-shinohara/weekly%20log/week-0/",
         "teaser": null
       }]
