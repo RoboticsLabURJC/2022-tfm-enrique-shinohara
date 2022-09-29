f9f6da7db8894b16b9b9642bc37af3d1a89f5f17
yujiro <YujiroS>
2022-09-29 10:11:34 +0200

    new entry on week 15 post

--- docs/_site/assets/js/lunr/lunr-store.js
+++ docs/_site/assets/js/lunr/lunr-store.js
@@ -64,4 +64,10 @@ var store = [{
         "tags": ["carla","scatter plot","augmentation"],
         "url": "/2022-tfm-enrique-shinohara/weekly%20log/week-14/",
         "teaser": null
+      },{
+        "title": "Week 15 - Building momentum",
+        "excerpt":"Finally, we have made some visible progress with the car learning how to follow the lane implicitly. To do this the idea is pretty much the same as the previous weeks, to enrich the dataset with more complex situations so that the car is able to handle the straights and...","categories": ["Weekly Log"],
+        "tags": ["carla"],
+        "url": "/2022-tfm-enrique-shinohara/weekly%20log/week-15/",
+        "teaser": null
       }]
